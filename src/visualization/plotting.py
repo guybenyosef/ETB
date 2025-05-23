0fe1485c2e5fd63a4f6d28a48cf5acfc16e4e069
Guy from Mac-Pro <gg.benyosef@gmail.com>
2025-05-23 18:11:18 +0300

    Refactor plot_curves function to accept a figure object and customizable title
    - Modified the plot_curves function to take a figure object as an argument.
    - Changed the way the plot is created by adding a subplot to the provided figure.
    - Added a parameter for customizable plot title, defaulting to 'Elastica in Tangent Bundle'.
    - Updated axis labeling and grid settings to use the new subplot approach.

--- src/visualization/plotting.py
+++ src/visualization/plotting.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
-def plot_curves(x, y, color):
-
-    plt.plot(x, y, color=color)
-    plt.xlabel('X-axis')
-    plt.ylabel('Y-axis')
-    plt.title('Elastica in Tangent Bundle')
-    plt.grid(True)
-    plt.axis('equal')
+def plot_curves(fig, x, y, color, label='Elastica in Tangent Bundle'):
+    ax = fig.add_subplot(111)
+    ax.plot(x, y, color=color)
+    ax.set_xlabel('X-axis')
+    ax.set_ylabel('Y-axis')
+    ax.set_title(label)
+    ax.grid(True)
+    ax.axis('equal')
     plt.show()
